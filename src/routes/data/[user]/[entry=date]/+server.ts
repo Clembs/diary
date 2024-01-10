60e3c5f797d0855606862234ab185dd4992060bb
Clément Voisin <clembs@clembs.com>
2024-01-10 19:06:41 +0100

    feat: get entry info endpoint

---
+++ src/routes/data/[user]/[entry=date]/+server.ts
@@ -0,0 +1,39 @@
+import { db } from '$lib/db';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ params }) => {
+	// const requestDate = new Date(params.entry);
+	const entry = await db.query.entries.findFirst({
+		// orderBy: ({ date }, { desc }) => desc(date),
+		where: ({ userId, date }, { eq, and }) => and(eq(userId, params.user), eq(date, params.entry)),
+		columns: {
+			date: true,
+			summary: true,
+			emoji: true,
+			emotion: true
+		},
+		with: {
+			activities: {
+				with: {
+					activity: {
+						columns: {
+							id: true,
+							label: true,
+							color: true
+						}
+					}
+				},
+				columns: {
+					percent: true
+				}
+			}
+		}
+	});
+
+	if (!entry) {
+		throw error(404, 'Entry not found');
+	}
+
+	return json(entry);
+};
